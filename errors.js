8d980824acb8645ef15f984e7e6be9f14af61733
zetxx <zetxx@belogradcik.biz>
2018-03-09 00:07:48 +0200

    fix: dep inject

--- errors.js
+++ errors.js
@@ -1,13 +1,12 @@
-const defineError = require('ut-error').define;
 var specificErrors = require('./map').specificErrors;
-const Aptra = defineError('aptra');
-const CommandReject = defineError(`commandReject`, Aptra, 'Command reject');
-const Timeout = defineError('timeout', Aptra, 'Transaction timed out');
-const Decode = defineError('decode', Aptra, 'No parser found');
-const UnknownMessageClass = defineError('unknownMessageClass', Aptra, 'Received unknown message class');
-
 
 module.exports = (defineError) => {
+    const Aptra = defineError('aptra');
+    const CommandReject = defineError('commandReject', Aptra, 'Command reject');
+    const Timeout = defineError('timeout', Aptra, 'Transaction timed out');
+    const Decode = defineError('decode', Aptra, 'No parser found');
+    const UnknownMessageClass = defineError('unknownMessageClass', Aptra, 'Received unknown message class');
+
     var errors = {
         aptra: Aptra,
         commandReject: CommandReject,
